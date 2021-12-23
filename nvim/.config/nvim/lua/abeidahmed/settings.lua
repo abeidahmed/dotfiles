e86e7a678eac0fb40f663d93a10f631533263b9c
abeidahmed <abeidahmed92@gmail.com>
2021-12-23 21:36:21 +0530

    Refactor opt

--- nvim/.config/nvim/lua/abeidahmed/settings.lua
+++ nvim/.config/nvim/lua/abeidahmed/settings.lua
@@ -1,28 +1,30 @@
-vim.opt.nu = true
-vim.opt.relativenumber = true
-vim.opt.hidden = true
-vim.opt.scrolloff = 8
-vim.opt.hlsearch = false
-vim.opt.termguicolors = true
-vim.opt.colorcolumn = '121'
-vim.opt.background = 'dark'
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
-vim.opt.cursorline = true
-vim.opt.smartindent = true
-vim.opt.wrap = false
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.writebackup = false
-vim.opt.incsearch = true
-vim.opt.signcolumn = 'yes'
-vim.opt.updatetime = 50
-vim.opt.clipboard = { 'unnamedplus', 'unnamed' }
-vim.opt.mouse = 'a'
-vim.opt.cmdheight = 2
-vim.opt.conceallevel = 0
-vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+local opt = vim.opt
 
-vim.opt.shortmess:append "c"
+opt.nu = true
+opt.relativenumber = true
+opt.hidden = true
+opt.scrolloff = 8
+opt.hlsearch = false
+opt.termguicolors = true
+opt.colorcolumn = '121'
+opt.background = 'dark'
+opt.expandtab = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.cursorline = true
+opt.smartindent = true
+opt.wrap = false
+opt.swapfile = false
+opt.backup = false
+opt.writebackup = false
+opt.incsearch = true
+opt.signcolumn = 'yes'
+opt.updatetime = 50
+opt.clipboard = { 'unnamedplus', 'unnamed' }
+opt.mouse = 'a'
+opt.cmdheight = 2
+opt.conceallevel = 0
+opt.completeopt = { 'menu', 'menuone', 'noselect' }
+
+opt.shortmess:append "c"
