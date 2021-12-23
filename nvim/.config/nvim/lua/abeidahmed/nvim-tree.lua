31d46ce07ce04bea645ace88368aa10dc7a3c288
abeidahmed <abeidahmed92@gmail.com>
2021-12-23 21:38:12 +0530

    Simplify nvim-tree config

--- nvim/.config/nvim/lua/abeidahmed/nvim-tree.lua
+++ nvim/.config/nvim/lua/abeidahmed/nvim-tree.lua
@@ -7,24 +7,11 @@ local list = {
 
 nvim_tree.setup {
   auto_close = true,
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-    ignore_list = {},
-  },
   view = {
     auto_resize = true,
     mappings = {
       list = list,
     },
-  },
-  quit_on_open = 0,
-  show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-    tree_width = 30,
   },
 }
 
