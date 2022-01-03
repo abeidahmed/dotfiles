d51c9807d13898e7de926ede73487e59223afdbb
abeidahmed <abeidahmed92@gmail.com>
2022-01-03 22:14:49 +0530

    Add hbs and lua comment style

--- nvim/.config/nvim/lua/abeidahmed/comment.lua
+++ nvim/.config/nvim/lua/abeidahmed/comment.lua
@@ -1,1 +1,6 @@
-require('Comment').setup()
+require('Comment').setup {}
+
+local ft = require('Comment.ft')
+
+ft.set('lua', { '--%s', '--[[%s]]' })
+ft.set('html.handlebars', { '{{!-- %s --}}', '{{!-- %s --}}' })
