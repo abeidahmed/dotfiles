4792b325ee650833fc001ff1740ac8c8adf73c99
abeidahmed <abeidahmed92@gmail.com>
2022-03-20 21:54:55 +0530

    change settings

--- nvim/.config/nvim/lua/abeidahmed/settings.lua
+++ nvim/.config/nvim/lua/abeidahmed/settings.lua
@@ -6,8 +6,8 @@ opt.hidden = true
 opt.scrolloff = 8
 opt.hlsearch = false
 opt.termguicolors = true
-opt.colorcolumn = '121'
-opt.background = 'dark'
+opt.colorcolumn = "121"
+opt.background = "dark"
 opt.expandtab = true
 opt.tabstop = 2
 opt.shiftwidth = 2
@@ -19,13 +19,33 @@ opt.swapfile = false
 opt.backup = false
 opt.writebackup = false
 opt.incsearch = true
-opt.signcolumn = 'yes'
-opt.updatetime = 50
-opt.clipboard = { 'unnamedplus', 'unnamed' }
-opt.mouse = 'a'
+opt.signcolumn = "yes"
+opt.updatetime = 1000
+opt.clipboard = { "unnamedplus", "unnamed" }
+opt.mouse = "n"
 opt.cmdheight = 2
 opt.conceallevel = 0
-opt.completeopt = { 'menu', 'menuone', 'noselect' }
-opt.guicursor = ''
+opt.completeopt = { "menu", "menuone", "noselect" }
+opt.guicursor = ""
+opt.splitright = true
+opt.belloff = "all"
+
+-- Cool floating window popup menu for completion on command line
+opt.pumblend = 17
+opt.wildmode = "longest:full"
+opt.wildoptions = "pum"
 
 opt.shortmess:append "c"
+
+opt.formatoptions = opt.formatoptions
+  - "a" -- Auto formatting is BAD.
+  - "t" -- Don't auto format my code. I got linters for that.
+  + "c" -- In general, I like it when comments respect textwidth
+  + "q" -- Allow formatting comments w/ gq
+  - "o" -- O and o, don't continue comments
+  + "r" -- But do continue when pressing enter.
+  + "n" -- Indent past the formatlistpat, not underneath it.
+  + "j" -- Auto-remove comments if possible.
+  - "2" -- I'm not in gradeschool anymore
+
+opt.joinspaces = false
