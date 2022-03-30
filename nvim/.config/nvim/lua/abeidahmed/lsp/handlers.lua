7c2c84794f547169bb2825574acdb22cec2455aa
abeidahmed <abeidahmed92@gmail.com>
2022-03-30 19:00:23 +0530

    feat: add border to diagnostic hover

--- nvim/.config/nvim/lua/abeidahmed/lsp/handlers.lua
+++ nvim/.config/nvim/lua/abeidahmed/lsp/handlers.lua
@@ -1,5 +1,27 @@
--- vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { width = 50 })
--- vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'single' })
+local signs = { Error = '', Warn = '', Info = '', Hint = '' }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+-- Root config for diagnostics
+vim.diagnostic.config {
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+  virtual_text = false,
+  float = {
+    style = "minimal",
+    border = "single",
+    source = "always",
+    focusable = false,
+    header = "",
+  },
+}
+
+vim.lsp.handlers["textdocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "single" })
+vim.lsp.handlers["textdocument/signaturehelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "single" })
 
 -- Jump directly to the first available definition every time.
 vim.lsp.handlers["textDocument/definition"] = function(_, result)
@@ -15,21 +37,15 @@ vim.lsp.handlers["textDocument/definition"] = function(_, result)
   end
 end
 
-local signs = { Error = '', Warn = '', Info = '', Hint = '' }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
-
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.handlers["textDocument/publishDiagnostics"], {
-  signs = true,
-  underline = true,
-  virtual_text = false,
-  severity_sort = true,
-  update_in_insert = false,
-  float = {
-    show_header = true,
-  },
-})
-
 vim.lsp.handlers["window/showMessage"] = require("abeidahmed.lsp.show-message")
+
+-- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.handlers["textDocument/publishDiagnostics"], {
+--   signs = true,
+--   underline = true,
+--   virtual_text = false,
+--   severity_sort = true,
+--   update_in_insert = false,
+--   float = {
+--     show_header = true,
+--   },
+-- })
