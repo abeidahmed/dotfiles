01645493e294505380d9ade5ce6c2d2b8afe2fd0
abeidahmed <abeidahmed92@gmail.com>
2022-06-08 20:32:03 +0400

    feat: null-ls format on save

--- nvim/.config/nvim/lua/abeidahmed/null-ls.lua
+++ nvim/.config/nvim/lua/abeidahmed/null-ls.lua
@@ -3,17 +3,61 @@ if not status_ok then
   return
 end
 
+local async_formatting = function(bufnr)
+  bufnr = bufnr or vim.api.nvim_get_current_buf()
+
+  vim.lsp.buf_request(
+    bufnr,
+    "textDocument/formatting",
+    { textDocument = { uri = vim.uri_from_bufnr(bufnr) } },
+    function(err, res, ctx)
+      if err then
+        local err_msg = type(err) == "string" and err or err.message
+        -- you can modify the log message / level (or ignore it completely)
+        vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
+        return
+      end
+
+      -- don't apply results if buffer is unloaded or has been modified
+      if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
+        return
+      end
+
+      if res then
+        local client = vim.lsp.get_client_by_id(ctx.client_id)
+        vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
+        vim.api.nvim_buf_call(bufnr, function()
+          vim.cmd("silent noautocmd update")
+        end)
+      end
+    end
+  )
+end
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup {
   sources = {
-    null_ls.builtins.formatting.prettier,
-    -- null_ls.builtins.formatting.eslint,
+    null_ls.builtins.formatting.prettier_d_slim,
+    null_ls.builtins.formatting.eslint_d,
     null_ls.builtins.formatting.gofmt,
-    null_ls.builtins.formatting.standardrb,
+    -- null_ls.builtins.formatting.standardrb,
     null_ls.builtins.formatting.rustfmt,
   },
-  on_attach = function(client)
+  on_attach = function(client, bufnr)
     if client.resolved_capabilities.document_formatting then
       vim.api.nvim_set_keymap("n", "<leader>lf", ":lua vim.lsp.buf.formatting()<CR>", { noremap = true })
+    end
+
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePost", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          async_formatting(bufnr)
+        end,
+      })
     end
   end,
 }
