89dbf00f82ccb6df9e60968c546f12e945e22156
abeidahmed <abeidahmed92@gmail.com>
2022-06-19 10:57:05 +0400

    specify list of servers to format automatically

--- nvim/.config/nvim/lua/abeidahmed/null-ls.lua
+++ nvim/.config/nvim/lua/abeidahmed/null-ls.lua
@@ -3,50 +3,33 @@ if not status_ok then
   return
 end
 
-local async_formatting = function(bufnr)
-  bufnr = bufnr or vim.api.nvim_get_current_buf()
-
-  vim.lsp.buf_request(
-    bufnr,
-    "textDocument/formatting",
-    { textDocument = { uri = vim.uri_from_bufnr(bufnr) } },
-    function(err, res, ctx)
-      if err then
-        local err_msg = type(err) == "string" and err or err.message
-        -- you can modify the log message / level (or ignore it completely)
-        vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
-        return
-      end
-
-      -- don't apply results if buffer is unloaded or has been modified
-      if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
-        return
-      end
-
-      if res then
-        local client = vim.lsp.get_client_by_id(ctx.client_id)
-        vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
-        vim.api.nvim_buf_call(bufnr, function()
-          vim.cmd("silent noautocmd update")
-        end)
-      end
-    end
-  )
-end
-
+local auto_format_servers = { "tsserver" }
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+local format = function(bufnr)
+  vim.lsp.buf.format {
+    filter = function(client)
+      for _, value in pairs(auto_format_servers) do
+        if client.name == value then
+          return true
+        end
+      end
+    end,
+    bufnr = bufnr,
+  }
+end
 
 null_ls.setup {
   sources = {
     null_ls.builtins.formatting.prettier_d_slim,
-    null_ls.builtins.formatting.eslint_d,
+    null_ls.builtins.formatting.eslint,
     null_ls.builtins.formatting.gofmt,
-    -- null_ls.builtins.formatting.standardrb,
+    null_ls.builtins.formatting.standardrb,
     null_ls.builtins.formatting.rustfmt,
   },
   on_attach = function(client, bufnr)
-    if client.server_capabilities.document_formatting then
-      vim.api.nvim_set_keymap("n", "<leader>lf", ":lua vim.lsp.buf.formatting()<CR>", { noremap = true })
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_set_keymap("n", "<leader>lf", ":lua vim.lsp.buf.format({ async = true })<CR>", { noremap = true })
     end
 
     if client.supports_method("textDocument/formatting") then
@@ -55,7 +38,7 @@ null_ls.setup {
         group = augroup,
         buffer = bufnr,
         callback = function()
-          async_formatting(bufnr)
+          format(bufnr)
         end,
       })
     end
