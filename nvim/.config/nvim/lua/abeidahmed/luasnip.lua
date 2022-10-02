3438d46ce2edc1003cb57467ef69e35ea2492c0e
abeidahmed <abeidahmed92@gmail.com>
2022-10-02 14:02:12 +0400

    extend html snippets on typescriptreact ft

--- nvim/.config/nvim/lua/abeidahmed/luasnip.lua
+++ nvim/.config/nvim/lua/abeidahmed/luasnip.lua
@@ -15,6 +15,7 @@ ls.config.setup {
 -- Files starting with `git` not a great idea
 ls.filetype_extend("gitcommit", { "gcommit" })
 ls.filetype_extend("handlebars", { "html" })
+ls.filetype_extend("typescriptreact", { "html" })
 
 -- Keymaps for luasnip
 vim.api.nvim_set_keymap("i", "<C-l>", "<Plug>luasnip-next-choice", {})
