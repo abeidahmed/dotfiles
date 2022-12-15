6e6de2e05eda98ac884a79cf189d0e5fb525cadc
abeidahmed <abeidahmed92@gmail.com>
2022-12-15 20:05:07 +0530

    disable markdown formatting

--- nvim/.config/nvim/lua/abeidahmed/null-ls.lua
+++ nvim/.config/nvim/lua/abeidahmed/null-ls.lua
@@ -24,7 +24,9 @@ end
 
 null_ls.setup {
   sources = {
-    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.prettierd.with {
+      disabled_filetypes = { "markdown" }
+    },
     null_ls.builtins.formatting.eslint_d,
     null_ls.builtins.formatting.gofmt,
     -- null_ls.builtins.formatting.standardrb,
