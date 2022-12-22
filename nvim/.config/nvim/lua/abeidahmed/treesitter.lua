66a67d4e45e7dc76ecd987536ab1b3d7b47a9fb7
abeidahmed <abeidahmed92@gmail.com>
2022-12-22 18:27:58 +0530

    add treesitter function jump mappings

--- nvim/.config/nvim/lua/abeidahmed/treesitter.lua
+++ nvim/.config/nvim/lua/abeidahmed/treesitter.lua
@@ -14,6 +14,24 @@ config.setup {
     enable = true,
     disable = { "yaml", "ruby" },
   },
+  textobjects = {
+    move = {
+      enabled = true,
+      set_jumps = true,
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+      }
+    }
+  },
   endwise = {
     enable = true,
   },
