e715b31e6565767e9720894398ba0c264ae49c7a
abeidahmed <abeidahmed92@gmail.com>
2023-07-12 14:07:39 +0400

    extend HTML snippets to eruby

--- nvim/.config/nvim/lua/abeidahmed/luasnip.lua
+++ nvim/.config/nvim/lua/abeidahmed/luasnip.lua
@@ -15,6 +15,7 @@ ls.config.setup {
 -- Files starting with `git` not a great idea
 ls.filetype_extend("gitcommit", { "gcommit" })
 ls.filetype_extend("handlebars", { "html" })
+ls.filetype_extend("eruby", { "html" })
 ls.filetype_extend("typescriptreact", { "html" })
 
 -- Keymaps for luasnip
