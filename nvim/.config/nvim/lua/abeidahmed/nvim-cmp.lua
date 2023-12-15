dadf80412a02035cf866a6d4f9ae5f424b28f5a8
abeidahmed <abeidahmed92@gmail.com>
2023-12-15 11:46:01 +0400

    decrease buffer autocomplete length

--- nvim/.config/nvim/lua/abeidahmed/nvim-cmp.lua
+++ nvim/.config/nvim/lua/abeidahmed/nvim-cmp.lua
@@ -102,7 +102,7 @@ cmp.setup {
     { name = "nvim_lsp" },
     { name = "path" },
     { name = "luasnip" },
-    { name = "buffer", keyword_length = 5 },
+    { name = "buffer", keyword_length = 3 },
   },
   snippet = {
     expand = function(args)
