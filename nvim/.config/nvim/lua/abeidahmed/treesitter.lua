5712a88ced0c54f080e9f5818f502ea12a38019b
abeidahmed <abeidahmed92@gmail.com>
2025-08-27 12:20:01 +0400

    Install missing treesitter grammar

--- nvim/.config/nvim/lua/abeidahmed/treesitter.lua
+++ nvim/.config/nvim/lua/abeidahmed/treesitter.lua
@@ -1,23 +1,42 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  main = "nvim-treesitter.configs", -- Sets main module to use for opts
-  dependencies = { 
-    "RRethy/nvim-treesitter-endwise"
-  },
-  opts = {
-    ensure_installed = { "bash", "diff", "html", "lua", "luadoc", "markdown", "markdown_inline", "query", "vim", "vimdoc", "ruby" },
-    auto_install = true,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = { "ruby" },
-    },
-    indent = {
-      enable = true,
-      disable = { "ruby" },
-    },
-    endwise = { 
-      enable = true,
-    },
-  },
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	main = "nvim-treesitter.configs", -- Sets main module to use for opts
+	dependencies = {
+		"RRethy/nvim-treesitter-endwise",
+	},
+	opts = {
+		ensure_installed = {
+			"bash",
+			"diff",
+			"html",
+			"lua",
+			"luadoc",
+			"markdown",
+			"markdown_inline",
+			"query",
+			"vim",
+			"vimdoc",
+			"ruby",
+			"typescript",
+			"javascript",
+			"vue",
+			"css",
+			"svelte",
+			"yaml",
+			"go",
+		},
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = { "ruby" },
+		},
+		indent = {
+			enable = true,
+			disable = { "ruby" },
+		},
+		endwise = {
+			enable = true,
+		},
+	},
 }
