530dd247dadc2cc1c34b5b48834d36a1b4e27f69
abeidahmed <abeidahmed92@gmail.com>
2026-05-12 10:09:11 +0400

    feat(vim-test): upgrade

--- nvim/.config/nvim/lua/abeidahmed/vim_test.lua
+++ nvim/.config/nvim/lua/abeidahmed/vim_test.lua
@@ -11,7 +11,38 @@ return {
 		vim.api.nvim_set_keymap("n", "<leader>rv", ":TestVisit<CR>", { noremap = true, silent = true })
 
 		vim.cmd([[
+      function! VimTestProjectRoot()
+        let l:markers = {
+          \ 'ruby': 'Gemfile.lock',
+          \ 'go': 'go.mod',
+          \ 'typescript': 'package.json',
+          \ 'typescriptreact': 'package.json',
+          \ 'javascript': 'package.json',
+          \ 'javascriptreact': 'package.json',
+          \ }
+        let l:marker = get(l:markers, &filetype, '')
+        if empty(l:marker)
+          return getcwd()
+        endif
+        let l:found = findfile(l:marker, '.;')
+        return empty(l:found) ? getcwd() : fnamemodify(l:found, ':p:h')
+      endfunction
+
       let test#strategy = "dispatch"
+      let test#project_root = function('VimTestProjectRoot')
+
+      function! VimTestUseBinTest() abort
+        let l:bin = findfile('bin/test', '.;')
+        if !empty(l:bin)
+          let l:abs = fnamemodify(l:bin, ':p')
+          let g:test#ruby#rails#executable = l:abs
+        endif
+      endfunction
+
+      augroup VimTestBinTest
+        autocmd!
+        autocmd BufEnter,BufRead,BufNewFile *.rb call VimTestUseBinTest()
+      augroup END
     ]])
 	end,
 }
