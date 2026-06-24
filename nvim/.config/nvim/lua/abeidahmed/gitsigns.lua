071a84d4ec2209bee9ce93f1970284c140f350e2
abeidahmed <abeidahmed92@gmail.com>
2026-06-24 11:14:59 +0400

    feat(gitsigns): add keymaps

--- nvim/.config/nvim/lua/abeidahmed/gitsigns.lua
+++ nvim/.config/nvim/lua/abeidahmed/gitsigns.lua
@@ -8,5 +8,40 @@ return {
 			topdelete = { text = "‾" },
 			changedelete = { text = "~_" },
 		},
+		on_attach = function(bufnr)
+			local gitsigns = require("gitsigns")
+
+			local function map(mode, l, r, opts)
+				opts = opts or {}
+				opts.buffer = bufnr
+				vim.keymap.set(mode, l, r, opts)
+			end
+
+			-- Navigation
+			map("n", "]c", function()
+				if vim.wo.diff then
+					vim.cmd.normal({ "]c", bang = true })
+				else
+					gitsigns.nav_hunk("next")
+				end
+			end)
+
+			map("n", "[c", function()
+				if vim.wo.diff then
+					vim.cmd.normal({ "[c", bang = true })
+				else
+					gitsigns.nav_hunk("prev")
+				end
+			end)
+
+			map("v", "<leader>hr", function()
+				gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+			end)
+			map("n", "<leader>hp", gitsigns.preview_hunk)
+			map("n", "<leader>hi", gitsigns.preview_hunk_inline)
+			map("n", "<leader>hb", function()
+				gitsigns.blame_line({ full = true })
+			end)
+		end,
 	},
 }
